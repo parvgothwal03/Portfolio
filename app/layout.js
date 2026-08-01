e3e5550aff7ea1645f12e6057ee5653b05eb5b79
parvgothwal03 <parvgothwal0310@gmail.com>
2026-08-01 11:26:24 +0530

    Header Competed

--- app/layout.js
+++ app/layout.js
@@ -20,7 +20,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-      className={`${outfit.className} ${ovo.className} antialiased`}>
+      className={`${outfit.className} ${ovo.className} antialiased leading-8 
+      overflow-x-hidden `}>
      {children}
      </body>
     </html>
